7355c50d99fc6b88bf3cc16f76fcc12f53e8e213
EclipseXuLu <249048056@qq.com>
2017-11-11 12:49:42 +0800

    improved input API

--- test/test.py
+++ test/test.py
@@ -17,18 +17,23 @@ def rotate_fm(fm, angle_range=[-30, 30]):
     result = []
 
     for _ in range(angle_range[0], angle_range[1], step):
-        M = cv2.getRotationMatrix2D((w / 2, h / 2), _, 1)
-        dst = cv2.warpAffine(np.transpose(fm, [2, 3, 1, 0]), M, (w, h))
-        result.append(dst)
+        r_fm = np.array([batch, c, h, w])
+        for each_bat_fm in r_fm[:, c, h, w]:
+            M = cv2.getRotationMatrix2D((w / 2, h / 2), _, 1)
+            dst = cv2.warpAffine(np.transpose(each_bat_fm, [2, 3, 1, 0]), M, (w, h))
+            result.append(dst)
 
     return result
 
 
 if __name__ == '__main__':
-    image = cv2.imread('/home/lucasx/Documents/talor.jpg')
-    # rotate_img(image, -15)
-    # result = feature_map_utils.mask_fm(image)
-    # for _ in result:
-    #     cv2.imshow('image', _)
-    #     cv2.waitKey(0)
-    #     cv2.destroyAllWindows()
+    if ',' in open('test.txt').read():
+        li = np.loadtxt('test.txt', delimiter=',')
+        print(li)
+    elif '\t' in open('test.txt').read():
+        li = np.loadtxt('test.txt', delimiter='\t')
+        print(li)
+    # image = cv2.imread('/home/lucasx/Documents/talor.jpg')
+    # fm = np.array([4, 3, None, None])
+    # fm[0][:][:][:] = image
+    # cv2.imshow('image', fm[0])
