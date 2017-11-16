f64ba0c1652233159394725a3ee917e7323ad878
EclipseXuLu <249048056@qq.com>
2017-11-16 12:20:24 +0800

    improved input API

---
+++ test/seq_name_fetcher.py
@@ -0,0 +1,17 @@
+import os
+import json
+from utils.config import *
+
+if __name__ == '__main__':
+    otb50_dir = configs['test_seq_base']
+
+    option = {"sequence": {"type": "OTB50", "seq_names": None}}
+
+    seq_name_list = []
+    for _ in os.listdir(otb50_dir):
+        seq_name_list.append(_)
+
+    option['sequence']['seq_names'] = seq_name_list
+
+    f = open('../tracking/options.json', mode='w')
+    json.dump(option, f)
